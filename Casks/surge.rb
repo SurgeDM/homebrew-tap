8e0fd352273140c32ed75521b890e828d2e27804
SuperCoolPencil <thesupercoolpencil@gmail.com>
2026-09-22 09:28:31 +0530

    fix: use non-deprecated cask postflight API

--- Casks/surge.rb
+++ Casks/surge.rb
@@ -33,7 +33,7 @@ cask "surge" do
 
   binary "surge"
 
-  postflight do
+  postflight_steps do
     if OS.mac?
       system_command "/usr/bin/xattr", args: ["-dr", "com.apple.quarantine", "#{staged_path}/surge"]
     end
